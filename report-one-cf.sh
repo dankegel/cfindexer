aeada49db15a50de1072b26c09740d17cfc4afeb
Dan Kegel <dank@kegel.com>
2018-08-18 20:21:40 -0400

    report-one-cf.sh: add kludgy workaround for bad date in one cis url

--- report-one-cf.sh
+++ report-one-cf.sh
@@ -15,8 +15,9 @@ then
          cisnc=$(echo "$line" | sed -E 's/.*\.pdf *//;s/ Neighborhood (Development )?Council//I;s/the //')
          # Extract date from filename
          # Make dates uniform; we could strip off the 20, or add it when missing...
+         # Add missing dash if needed (e.g. 14-0656-S9_cis_12-1315.pdf should be 14-0656-S9_cis_12-13-15.pdf)
          # And, ugh, zero-extend.
-         cisdate=$(echo "$cisurl" | sed -E 's/[a-z]?\.pdf//I;s/.*_//' | sed -E 's/^([0-9])-/0\1-/;s/-([0-9])-/-0\1-/;s/-([0-6][0-9])$/-20\1/')
+         cisdate=$(echo "$cisurl" | sed -E 's/[a-z]?\.pdf//I;s/.*_//' | sed -E 's/^([0-9][0-9])-([0-9][0-9])([0-9][0-9])$/\1-\2-\3/' | sed -E 's/^([0-9])-/0\1-/;s/-([0-9])-/-0\1-/;s/-([0-6][0-9])$/-20\1/')
          # Date is M/D/Y but swizzle it to Y/M/D to be sortable
          # sorttable.js has date sorting, but it's broken, so I disabled it
          cisdate=$(echo "$cisdate" | awk -F- '{print $3"-"$1"-"$2}')
