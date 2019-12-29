8043ebf8376377c696f2a297e81152c6b8cbd953
Dan Kegel <dank@kegel.com>
2019-12-29 17:34:55 -0500

    Added report-topics a year ago and didn't bother checking in the call... tsk.

--- report.sh
+++ report.sh
@@ -5,4 +5,5 @@ SRCDIR=$(cd $(dirname $0); pwd)
 sh -x $SRCDIR/report-all.sh
 sh -x $SRCDIR/report-cis.sh
 sh -x $SRCDIR/report-committee.sh
+sh -x $SRCDIR/report-topics.sh
 sh -x $SRCDIR/report-index.sh
