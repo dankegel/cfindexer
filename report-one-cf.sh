0558d3fc6cd9e01e4f13e083a481137dd4ba3691
Dan Kegel <dank@kegel.com>
2019-12-30 12:24:37 -0500

    report-one-cf.sh: handle tons of little variants in nc name field

--- report-one-cf.sh
+++ report-one-cf.sh
@@ -3,9 +3,27 @@
 # If arg 2 is --cisonly, only output a row if there is no CIS.
 
 # Reads name of NC on stdin, outputs tidied name on stdout
+# See test cases in nc-variants.txt
 tidy_nc() {
-   sed -E 's/ (Neighborhood|Neighbhood|Neighborghood|Neighborhodd|Neighborhood|Neighbrohood|Neighhborhood|Neughborhood|Community) (of Historic Arlington Heights)?(Development )?Cou[ncil]*//Ig;s/NDC$//;s/NC$//;s/the //'
-
+   # Handle typos and variants
+   # Remove prefixes/suffixes like NC, NDC, Council of, Council, Development Council, or Neighborhood Council
+   # Remove prefixes like by or the
+   # Remove duplicated phases separated by commas (go figure)
+   # Remove keywords e, 1st Submittal, or 2nd Submittal
+   neighborhood='Neighborhood|Neighbhood|Neighborghood|Neighborhodd|Neighborhoos|Neighbrohood|Neighhborhood|Neightborhood|Neughborhood'
+   sed -E 's/(Bel Air Beverly Crest|Bel-Air Beverly Crest)/Bel Air-Beverly Crest/' |
+   sed -E 's/(NoHo|Mid-Town North Hollywood|Mid Town North Hollywood)/North Hollywood/' |
+   sed 's/Greater Echo Park Elysian/Echo Park/' |
+   sed 's/Foothills/Foothill/;s/Glassel /Glassell /;s/Hils/Hills/;s/Hollwood/Hollywood/' |
+   sed 's/Hostoric/Historic/;s/Playa Del Rey/Playa del Rey/;s/VIllage/Village/' |
+   sed 's/United Neighborhoods of the Historic Arlington Heights/United Neighborhoods/' |
+   sed 's/Voices of 90037/Voices/;s/WEST/West/' |
+   sed 's/Wilshire Center-Koreatown/Wilshire Center Koreatown/' |
+   sed 's/Woodland Hills-Warner Center/Woodland Hills Warner Center/' |
+   sed -E "s/((Community|$neighborhood) )?(Development )?(Councils? ?)?(of )?//Ig" |
+   sed 's/NDC//;s/NC//;s/by //;s/the //' |
+   sed 's/\<\([A-Z a-z][A-Z a-z]*\) *,\1\>/\1/g' |
+   sed 's/(e)//;s/(1st Submittal)//;s/(2nd Submittal)//;s/ - 2nd Submission//'
 }
 
 cf=$1
