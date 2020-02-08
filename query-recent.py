3452bfd9a549918f7da6b1e6003c48686caae4ae
Dan Kegel <dank@kegel.com>
2020-02-07 20:32:58 -0800

    query-recent.py: batch queries by month to avoid overload

--- query-recent.py
+++ query-recent.py
@@ -5,6 +5,24 @@
 # Output: prints one council file number per line to stdout
 
 # See https://www.dataquest.io/blog/web-scraping-beautifulsoup/
+#
+# Frequently observed action types:
+# 2 referred
+# 4 Continued
+# 5 document submitted
+# 7 Scheduled for committee
+# 14 Approved
+# 18 Scheduled for council
+# 22 community impact statment
+# 25 referred - motion
+# 34 introduced (rare)
+# 44 Council adopted
+# 45 Council action final
+# 46 Council adopted forthwith
+# 50 Expired file
+
+# The action types we probably care about:
+actions = [2, 5, 7, 18, 22, 25, 34]
 
 import datetime
 import re
@@ -13,20 +31,18 @@ import time
 from bs4 import BeautifulSoup
 
 now = datetime.date.today()
-week = datetime.timedelta(weeks=1)
-last_week = (now - week).strftime('%m/%d/%Y')
-next_week = (now + week).strftime('%m/%d/%Y')
+month = datetime.timedelta(days=31)
 qurl = 'https://cityclerk.lacity.org/lacityclerkconnect/index.cfm?fa=vcfi.doSearch'
 datafmt = "btnAdvanced=Search&searchform=advanced&ActionTaken=%s&ActDateStart=%s&ActDateEnd=%s"
 headers = {"Content-Type": "application/x-www-form-urlencoded"}
-# 2 referred
-# 5 document submitted
-# 22 community impact statment
-# 25 referred - motion
-# 34 introduced (rare)
 cfnums = {}
-for action in [2, 5, 22, 25, 34]:
-   data=datafmt % (action, last_week, next_week)
+
+# Can't do more than about a month without it clipping some results
+for monthsago in range(1, 4):
+ tstart = (now - monthsago * month).strftime('%m/%d/%Y')
+ tend = (now - (monthsago-1) * month).strftime('%m/%d/%Y')
+ for action in actions:
+   data=datafmt % (action, tstart, tend)
    response = requests.post(qurl, data=data, headers=headers)
    soup = BeautifulSoup(response.text, 'html.parser')
    links = soup.find_all(href=re.compile("cfnumber"))
